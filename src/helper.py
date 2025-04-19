168bdda761ee6135010094dd47a2d7d483264ab2
thparas07 <parasth9@gmail.com>
2025-04-19 21:38:39 +0530

    new data 19april

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 
 
 
